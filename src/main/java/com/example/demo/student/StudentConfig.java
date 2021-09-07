1f8ed4122d16e8428f6e4312249491aeb7ba71b7
hasithaudara <hasitha.u.bombuwala@gmail.com>
2021-09-07 10:51:31 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "mariam",
+                    "mariamJ@gmail.com",
+                    LocalDate.of(2000, JANUARY, 6)
+            );
+
+            Student alex = new Student(
+                    "alex",
+                    "alexTJ@gmail.com",
+                    LocalDate.of(2004, JANUARY, 3)
+            );
+
+            repository.saveAll(
+                    List.of(mariam,alex)
+            );
+        };
+    }
+}
